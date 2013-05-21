e9d04a76d0732f6f43d3a2d6a957fd880622a515
Andre Venancio <venancio.andre@gmail.com>
2013-05-21 17:25:35 +0100

    simple noise

--- src/noise/razor.simple.js
+++ src/noise/razor.simple.js
@@ -4,7 +4,7 @@
  */
 
 /**
- * [ description]
+ * Simple implementation of noise.
  * @param  {Number} w Width.
  * @param  {Number} h Height.
  * @return {imageData}   Manipulated image data.
@@ -13,16 +13,33 @@ Razor.SimpleNoise = function(w, h) {
   this.width = w || 50;
   this.height = h || 50;
 
-  this.noise = document.createElement('canvas');
-  this.simple = document.createElement('canvas');
+  this.whiteNoiseCanvas = document.createElement('canvas');
+  this.simpleNoiseCanvas = document.createElement('canvas');
 
-  this.contextNoise = this.noise.getContext('2d');
-  this.contextSimple = this.simple.getContext('2d');
+  this.whiteNoiseCanvas.width = this.simpleNoiseCanvas.width = this.width;
+  this.whiteNoiseCanvas.height = this.simpleNoiseCanvas.height = this.height;
+
+  this.contextNoise = this.whiteNoiseCanvas.getContext('2d');
+  this.contextSimple = this.simpleNoiseCanvas.getContext('2d');
 
   this.saved_alpha = this.contextSimple.globalAlpha;
-  this.noise.width = this.width;
-  this.noise.height = this.height;
 
+  this.init();
+};
+
+/**
+ * initializes Simple Noise
+ */
+Razor.SimpleNoise.prototype.init = function() {
+  this.buildWhiteNoise();
+  this.buildSimpleNoise();
+};
+
+/**
+ * Builds white noise as a base for the simple noise.
+ * TODO: try to build simple noise from a given image
+ */
+Razor.SimpleNoise.prototype.buildWhiteNoise = function() {
   var noiseImageData = this.contextSimple.createImageData(this.width, this.height);
   var pixels = noiseImageData.data;
 
@@ -34,15 +51,26 @@ Razor.SimpleNoise = function(w, h) {
   }
 
   this.contextNoise.putImageData(noiseImageData, 0, 0);
+};
 
+/**
+ * Builds Simple Noise.
+ */
+Razor.SimpleNoise.prototype.buildSimpleNoise = function() {
   for (var size = 4; size <= this.width; size *= 2) {
     var x = Math.floor(Math.random() * (this.width - size));
     var y = Math.floor(Math.random() * (this.height - size));
 
     this.contextSimple.globalAlpha = 4 / size;
-    this.contextSimple.drawImage(this.noise, x, y, size, size, 0, 0, this.width, this.height);
+    this.contextSimple.drawImage(this.whiteNoiseCanvas, x, y, size, size, 0, 0, this.width, this.height);
   }
   this.contextSimple.globalAlpha = this.saved_alpha;
+};
 
+/**
+ * Returns generated noise.
+ * @return {ImageData} Resulting ImageData Object.
+ */
+Razor.SimpleNoise.prototype.getNoise = function() {
   return this.contextSimple.getImageData(0, 0, this.width, this.height);
 };
